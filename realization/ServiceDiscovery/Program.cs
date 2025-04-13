22c64c171cf786b3fe4c3149afcbf6e0d91222b3
Гурин Артур <arturgurin39@gmail.com>
2025-04-13 13:34:23 +0300

    base_realization: Добавлены правила написания кода

--- realization/ServiceDiscovery/Program.cs
+++ realization/ServiceDiscovery/Program.cs
@@ -18,6 +18,8 @@ if (app.Environment.IsDevelopment())
 
 app.MapGroup("/services")
     .MapServicesApi()
-    .WithTags("Public"); 
+    .WithTags("Public");
+
+app.UseGlobalExceptionHandler();
 
 app.Run();
--- realization/ServiceDiscovery/ServiceRegistry/IServiceRegistry.cs
+++ realization/ServiceDiscovery/ServiceRegistry/IServiceRegistry.cs
@@ -5,12 +5,12 @@ namespace API.ServiceRegistry;
 public interface IServiceRegistry
 {
     void Register(ServiceInfo service);
-    
+
     bool TryGet(Guid id, out ServiceInfo service);
-    
+
     ICollection<ServiceInfo> GetAll();
-    
+
     bool Unregister(Guid id);
-    
+
     bool Update(ServiceInfo service);
 }
