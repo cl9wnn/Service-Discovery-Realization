d4dc922c6df0cf8a46e76bb99805ab0f9ff84eab
Egor <egor_udir@mail.ru>
2025-05-03 20:06:46 +0300

    сервисы регистрируются как локально, так и в докере, в зависимости от среды

--- realization/ServiceDiscovery/Program.cs
+++ realization/ServiceDiscovery/Program.cs
@@ -9,18 +9,29 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IServiceRegistry, RedisServiceRegistry>();
 builder.Services.AddSingleton<IHealthChecker, HealthChecker>();
 builder.Services.AddMappings();
 builder.Services.AddHttpClient();
 builder.Services.AddLogging();
 
+if (!builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSingleton<IConnectionMultiplexer>(
+        _ => ConnectionMultiplexer.Connect("redis:6379"));
+}
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddSingleton<IServiceRegistry, InMemoryServiceRegistry>();
+}
+else
+{
+    builder.Services.AddSingleton<IServiceRegistry, RedisServiceRegistry>();
+}
+
 builder.Services.AddHangfire(config => config.UseMemoryStorage());
 
 builder.Services.AddHangfireServer();
-
-builder.Services.AddSingleton<IConnectionMultiplexer>(
-    _ => ConnectionMultiplexer.Connect("redis:6379"));
 
 var app = builder.Build();
 app.UseHangfireDashboard();
